3eea214aeefe46797f948629957a13c2677081e7
Tobias Lisbakken <tobman@live.no>
2024-03-30 18:55:19 +0100

    clean code?

--- plugin/nerdtree.lua
+++ plugin/nerdtree.lua
@@ -1,25 +1,42 @@
+local logger = require("structlog").get_logger("default")
+
+
 vim.api.nvim_set_keymap('n','<C-q>',':lua NerdOpen()<CR>',{ noremap = true, silent = true });
 vim.api.nvim_set_keymap('n','<C-f>',':NERDTreeFocus<CR>', { noremap = true, silent = true });
 
 
-function NerdOpen()
+local function get_nerdtree_window()
+  local windows = vim.api.nvim_list_wins();
 
-  local nerdtree_buffer_name = "NERD_tree"
-
-  for _,win in ipairs(vim.api.nvim_list_wins()) do
-    local curr = vim.api.nvim_get_current_win()
+  for _,win in pairs(windows) do
     local buf = vim.api.nvim_win_get_buf(win)
-    local buf_name = vim.api.nvim_buf_get_name(buf)
+    local name = vim.api.nvim_buf_get_name(buf)
 
-
-    if buf_name:find(nerdtree_buffer_name) then
-      if win == curr then vim.cmd(":NERDTreeClose")
-      else vim.cmd(":NERDTreeFocus") end
-      return
+    if name:find("NERD_tree") then
+      return win
     end
   end
 
-  vim.cmd(":NERDTree");
+  return nil
+end
 
+function NerdOpen()
+  local current_window = vim.api.nvim_get_current_win()
+  local nerdtree_window = get_nerdtree_window()
+
+  logger:info(string.format("current: %s nerdtree: %s", current_window, nerdtree_window))
+
+  if nerdtree_window == nil then
+    logger:info("No nerdtree open")
+    vim.cmd(":NERDTree")
+  else
+    if nerdtree_window == current_window then
+      vim.cmd(":NERDTreeClose")
+      logger:info("Closing nerd tree")
+    else
+      vim.cmd("NERDTreeFocus")
+      logger:info("Focusing nerd tree")
+    end
+  end
 end
 
