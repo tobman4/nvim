c0ec5c714544bfd59af87478ad07af46abe1a5e0
tl <tobman@live.no>
2025-07-09 00:27:20 +0200

    git plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,7 +6,18 @@ return {
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-path",
 	"hrsh7th/cmp-cmdline",
-	"hrsh7th/nvim-cmp",
-	"onsails/lspkind.nvim"
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "onsails/lspkind.nvim" }
+	},
+
+	{
+  	'tanvirtin/vgit.nvim',
+  	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+  	-- Lazy loading on 'VimEnter' event is necessary.
+  	event = 'VimEnter',
+  	config = function() require("vgit").setup() end,
+	}
+
 
 }
