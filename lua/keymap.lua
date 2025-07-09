5c9dfe83224339ccf8e29fa74c3791639319cc62
tl <tobman@live.no>
2025-07-09 18:52:37 +0200

    git keys

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,3 +1,11 @@
 -- Telescope
 vim.keymap.set("n", "<C-f>", ":Telescope find_files<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true })
+<<<<<<< Updated upstream
+=======
+
+-- GIT
+vim.keymap.set("n", "<leader>gg", ":VGit project_diff_preview<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>gl", ":VGit project_logs_preview<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>gh", ":VGit buffer_history_preview<CR>", { noremap = true })
+>>>>>>> Stashed changes
