727ae1cf7b4f110e300bf0f6535bfded80d40053
tobman <tl@braum.tobman.no>
2025-10-23 19:19:01 +0200

    sexy

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -5,6 +5,14 @@ require("telescope").setup({
         ["<C-s>"] = "file_vsplit"
       }
     },
+		layout_config = {
+			prompt_position = "top"
+		},
+		pickers = {
+			find_files = {
+				theme = "dropdown"
+			}
+		},
     file_ignore_patterns = {
       "__pycache__",
       "%.env",
