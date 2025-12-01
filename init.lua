c96a7cd86911f6b7990f02d05d07d32c403410d5
tl <tobman@live.no>
2025-12-01 14:22:12 +0100

    fix indent

--- init.lua
+++ init.lua
@@ -7,25 +7,16 @@ require("keymap")
 require("lsp")
 require("lsp.lua")
 require("lsp.dotnet")
-require("lsp.ts")
 
 -- Settings
 vim.o.tabstop = 2
+vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+vim.o.expandtab = true
 vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
 
 vim.o.mouse = ""
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "python",
-	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.softtabstop = 2
-		vim.bo.expandtab = true
-	end
-})
 
 vim.cmd("colorscheme deus")
