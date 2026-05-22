7840ee8d404714e3ecb390558e865e9c0d24dd36
tobman4 <36448579+tobman4@users.noreply.github.com>
2026-05-22 21:13:34 +0000

    🎨 Palette: Enable bordered windows for nvim-cmp to improve visual hierarchy

--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -7,8 +7,8 @@ cmp.setup {
 		{ name = "buffer" }
 	},
 	window = {
-  	-- completion = cmp.config.window.bordered(),
-  	-- documentation = cmp.config.window.bordered(),
+	completion = cmp.config.window.bordered(),
+	documentation = cmp.config.window.bordered(),
   },
 	mapping = cmp.mapping.preset.insert({
 		["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
