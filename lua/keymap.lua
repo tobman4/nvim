2e73f30bc206034c29bc240c445649d49654e73d
tobman4 <36448579+tobman4@users.noreply.github.com>
2026-05-22 13:23:35 +0000

    feat: add nvim-tree file explorer
    - Added `nvim-tree/nvim-tree.lua` and its devicons dependency to `lua/plugins.lua`.
    - Configured the tree to open on the right side.
    - Mapped `<C-f>` to `:NvimTreeToggle<CR>` in `lua/keymap.lua`, replacing the redundant telescope mapping.

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,5 +1,7 @@
+-- File Explorer
+vim.keymap.set("n", "<C-f>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
 -- Telescope
-vim.keymap.set("n", "<C-f>", ":Telescope find_files<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true })
 
 -- GIT
