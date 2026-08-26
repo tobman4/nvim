7954ba8d49d42fc3f2a8bcd3c62461d4343aacd7
tobman4 <36448579+tobman4@users.noreply.github.com>
2026-08-26 12:10:27 +0000

    Fix metatable usage in lua/logging.lua

--- lua/logging.lua
+++ lua/logging.lua
@@ -4,7 +4,7 @@ local Logger = {}
 Logger.__index = Logger
 
 function Logger:new(name)
-  local instance = setmetatable(Logger, {});
+  local instance = setmetatable({}, self);
 
   instance.name = name
 
